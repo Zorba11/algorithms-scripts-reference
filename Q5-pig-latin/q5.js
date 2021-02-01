23d00c9ad1e0790e376cb25afc90bdcda4f9353a
Zorba11 <zorbathegreek111@gmail.com>
2021-02-01 21:40:46 +0530

    q5 solved

---
+++ Q5-pig-latin/q5.js
@@ -0,0 +1,36 @@
+function translatePigLatin(str) {
+    
+   let firstLetter = str[0];
+   if (/^[aeiou]$/i.test(firstLetter)) {
+    return str + 'way';
+   }
+
+   let strArray = str.split('');
+//    console.log(strArray);
+let counter = 0;
+    for(let singleStr of strArray) {
+        // console.log(singleStr);
+
+        
+
+        if((/^[aeiou]$/i).test(singleStr)) {
+            counter++;
+            
+        }
+        if (counter === 1) {
+            // console.log('solu-',strArray.splice(str.indexOf(singleStr)).join(''));
+            let firstHalf = strArray.splice(str.indexOf(singleStr)).join('');
+            let secondHalf = strArray.splice(0, str.indexOf(singleStr)).join('');
+            
+            return firstHalf + secondHalf + 'ay';
+        }
+
+        // console.log('indexof', str.indexOf('o'))
+    }
+
+    if (counter === 0) return str + 'ay';
+
+  }
+  
+//   console.log(translatePigLatin("glove")) //should return "oveglay"
+console.log(translatePigLatin("rhythm")) // should return "rhythmay"
